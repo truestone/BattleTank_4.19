415d60ea919f17521b000416c676daca0acf528c
Jinseok Seo <truestone@gmail.com>
2020-05-09 20:18:48 +0900

    BT274. Introducing Particle Systems (실습 60: 파티클 시스템)

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -28,9 +28,9 @@ public:
 private:
     UProjectileMovementComponent* ProjectileMovement = nullptr;
 
-    UPROPERTY(VisibleAnywhere, Category = "Component")
+    UPROPERTY(VisibleAnywhere, Category = "Components")
     UStaticMeshComponent* CollisionMesh = nullptr;
 
-    UPROPERTY(VisibleAnywhere, Category = "Component")
+    UPROPERTY(VisibleAnywhere, Category = "Components")
     UParticleSystemComponent* LaunchBlast = nullptr;
 };
--- README.md
+++ README.md
@@ -73,4 +73,5 @@
 * BT269. Making an Ammo Display (실습 57: 탄환 수 표시)
 * BT270. Making an AutoMortar (실습 58: 포탑 만들기)
 * BT273. Preparing for Particles (실습 59: 발사체 수정)
+* BT274. Introducing Particle Systems (실습 60: 파티클 시스템)
 

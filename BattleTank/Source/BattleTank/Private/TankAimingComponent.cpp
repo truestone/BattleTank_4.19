b4dfd421be906dfc3b00a0aead36f477c65a4274
Jinseok Seo <truestone@gmail.com>
2020-05-09 15:13:57 +0900

    BT269. Making an Ammo Display (실습 57: 탄환 수 표시)

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -27,7 +27,11 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-    if ((FPlatformTime::Seconds() - LastFireTime) < ReloadTimeInSeconds)
+    if (RoundsLeft <= 0)
+    {
+        FiringState = EFiringState::OutOfAmmo;
+    }
+    else if ((FPlatformTime::Seconds() - LastFireTime) < ReloadTimeInSeconds)
     {
         FiringState = EFiringState::Reloading;
     }
@@ -85,6 +89,11 @@ EFiringState UTankAimingComponent::GetFiringState() const
     return FiringState;
 }
 
+int UTankAimingComponent::GetRoundsLeft() const
+{
+    return RoundsLeft;
+}
+
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 {
     auto BarrelRotator = Barrel->GetForwardVector().Rotation();
@@ -101,7 +110,7 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 
 void UTankAimingComponent::Fire()
 {
-    if (FiringState != EFiringState::Reloading)
+    if (FiringState == EFiringState::Locked || FiringState == EFiringState::Aiming)
     {
         if (!ensure(Barrel)) return;
 
@@ -117,5 +126,7 @@ void UTankAimingComponent::Fire()
 
         Projectile->LaunchProjectile(LaunchSpeed);
         LastFireTime = FPlatformTime::Seconds();
+
+        RoundsLeft--;
     }
 }
--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -11,7 +11,8 @@ enum class EFiringState : uint8
 {
     Reloading,
     Aiming,
-    Locked
+    Locked,
+    OutOfAmmo
 };
 
 class UTankBarrel;
@@ -35,6 +36,8 @@ public:
     UFUNCTION(BlueprintCallable, Category = Firing)
     void Fire();
 
+    UFUNCTION(BlueprintCallable, Category = Firing)
+    int GetRoundsLeft() const;
 
 protected:
 	// Called when the game starts
@@ -67,4 +70,6 @@ private:
     double LastFireTime = 0;
 
     FVector AimDirection;
+
+    int RoundsLeft = 3;
 };
